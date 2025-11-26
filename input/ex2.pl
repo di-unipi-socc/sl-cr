3086b71432cc613d38cb41b5bbbb57fee1c8c1dd
Stefano <stefano.forti92@gmail.com>
2025-11-26 16:24:17 +0100

    first commit

---
+++ input/ex2.pl
@@ -0,0 +1,101 @@
+%%% query:
+% initial_state(S), crSep(S, Sok, Sko).
+% initial_state(S), cr(S, Sok, SkoFixed, SFinal).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Nodes (100)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+node(n1).  node(n2).  node(n3).  node(n4).  node(n5).
+node(n6).  node(n7).  node(n8).  node(n9).  node(n10).
+node(n11). node(n12). node(n13). node(n14). node(n15).
+node(n16). node(n17). node(n18). node(n19). node(n20).
+node(n21). node(n22). node(n23). node(n24). node(n25).
+node(n26). node(n27). node(n28). node(n29). node(n30).
+node(n31). node(n32). node(n33). node(n34). node(n35).
+node(n36). node(n37). node(n38). node(n39). node(n40).
+node(n41). node(n42). node(n43). node(n44). node(n45).
+node(n46). node(n47). node(n48). node(n49). node(n50).
+node(n51). node(n52). node(n53). node(n54). node(n55).
+node(n56). node(n57). node(n58). node(n59). node(n60).
+node(n61). node(n62). node(n63). node(n64). node(n65).
+node(n66). node(n67). node(n68). node(n69). node(n70).
+node(n71). node(n72). node(n73). node(n74). node(n75).
+node(n76). node(n77). node(n78). node(n79). node(n80).
+node(n81). node(n82). node(n83). node(n84). node(n85).
+node(n86). node(n87). node(n88). node(n89). node(n90).
+node(n91). node(n92). node(n93). node(n94). node(n95).
+node(n96). node(n97). node(n98). node(n99). node(n100).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Components (10)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+component(c1).
+component(c2).
+component(c3).
+component(c4).
+component(c5).
+component(c6).
+component(c7).
+component(c8).
+component(c9).
+component(c10).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Hardware Requests
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+req_hw(c1, 1).
+req_hw(c2, 2).
+req_hw(c3, 3).
+req_hw(c4, 4).
+req_hw(c5, 1).
+req_hw(c6, 2).
+req_hw(c7, 3).
+req_hw(c8, 4).
+req_hw(c9, 5).
+req_hw(c10,6).
+
+initial_state(s(P, R)) :-
+    placement(P), caplist(R).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Node capacities
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+caplist([
+  r(n1,5), r(n2,4), r(n3,6), r(n4,3), r(n5,8),
+  r(n6,5), r(n7,4), r(n8,7), r(n9,6), r(n10,5),
+  r(n11,4), r(n12,7), r(n13,3), r(n14,8), r(n15,6),
+  r(n16,5), r(n17,4), r(n18,7), r(n19,6), r(n20,5),
+  r(n21,4), r(n22,8), r(n23,5), r(n24,6), r(n25,7),
+  r(n26,4), r(n27,6), r(n28,5), r(n29,8), r(n30,3),
+  r(n31,7), r(n32,5), r(n33,6), r(n34,4), r(n35,8),
+  r(n36,5), r(n37,6), r(n38,4), r(n39,7), r(n40,5),
+  r(n41,6), r(n42,4), r(n43,5), r(n44,7), r(n45,8),
+  r(n46,3), r(n47,5), r(n48,6), r(n49,7), r(n50,4),
+  r(n51,8), r(n52,6), r(n53,4), r(n54,5), r(n55,7),
+  r(n56,6), r(n57,4), r(n58,8), r(n59,5), r(n60,7),
+  r(n61,4), r(n62,5), r(n63,7), r(n64,6), r(n65,8),
+  r(n66,4), r(n67,6), r(n68,5), r(n69,7), r(n70,4),
+  r(n71,8), r(n72,6), r(n73,5), r(n74,7), r(n75,4),
+  r(n76,6), r(n77,5), r(n78,7), r(n79,4), r(n80,8),
+  r(n81,6), r(n82,5), r(n83,7), r(n84,4), r(n85,8),
+  r(n86,6), r(n87,5), r(n88,7), r(n89,4), r(n90,8),
+  r(n91,6), r(n92,5), r(n93,7), r(n94,4), r(n95,8),
+  r(n96,6), r(n97,5), r(n98,7), r(n99,4), r(n100,8)
+]).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Placement (intentionally unbalanced)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+placement([
+  c(c1,n3),
+  c(c2,n3),
+  c(c3,n3),    % n3 overloaded (6 capacity, 1+2+3 = 6 OK, but adding c4 below breaks)
+  c(c4,n3),
+  c(c5,n10),
+  c(c6,n20),
+  c(c7,n21),
+  c(c8,n50),
+  c(c9,n70),
+  c(c10,n70)   % n70 overloaded (capacity 4, requires 5+6)
+]).
---
+++ main.pl
@@ -0,0 +1,119 @@
+:- set_prolog_flag(stack_limit, 20_000_000_000).
+
+%%%%%%%%%%%%%% Infix ★ star operator %%%%%%%%%%%%%%%%%%%%%%%
+:- op(500, xfy, ★). 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+
+wf(State) :- deploymentOk(State), nodes(Ns), nodesOk(State).
+
+% checks deployment policies for all components
+deploymentOk(s(P,_)) :-
+    findall(c(C,N), member(c(C,N), P), Cs), deploymentOk(Cs, P).
+
+deploymentOk([c(C,N)|Cs], P) :-
+    delta(C, N), deploymentOk(Cs, P).
+deploymentOk([], _).
+
+% checks node capacity constraints for all nodes
+nodesOk(State) :- nodes(Ns), nodesOk(Ns, State).
+
+nodesOk([N|Ns], State) :-
+    nodeOk(N, State), nodesOk(Ns, State).
+nodesOk([], _).
+
+nodeOk(N, s(P,R)) :-
+    findall(H, (member(c(C,N), P), req_hw(C, H)), Hs),
+    sum_list(Hs, Used), cap(N, R, Cap), Used =< Cap.
+
+% checks that σ = σ1 \oplus σ2 holds
+sep(s(P,R), s(P1,R1), s(P2,R2)) :-
+    ground(P1), ground(R1), ground(P2), ground(R2), 
+    ord_union(P1, P2, P),
+    ord_intersection(P1, P2, []),
+    checkCap(R, s(P1,R1), s(P2,R2)).
+% generates separations    
+sep(s(P,R), s(P1,R1), s(P2,R2)) :-
+    part(P, P1, P2), split(R, R1, R2). 
+
+% checks capacity constraints for separated states
+checkCap(R, s(_P1,R1), s(_P2,R2)) :- nodes(Ns), checkNodesCap(Ns, R, R1, R2).
+
+checkNodesCap([N|Ns], R, R1, R2) :-
+    cap(N, R,  Cap), cap(N, R1, Cap1), cap(N, R2, Cap2),
+    Cap1 + Cap2 =< Cap,
+    checkNodesCap(Ns, R, R1, R2).
+checkNodesCap([], _, _, _).
+
+% separating conjunction
+holds(F1 ★ F2, S) :-
+    sep(S, S1, S2),
+    holds(F1, S1),
+    holds(F2, S2).
+
+% holds(wf ★ wf, s([c(c1,n1),c(c2,n2)], [r(n1,100), r(n2,100)]), S1, S2).
+holds(F1 ★ F2, S, S1, S2) :-
+    sep(S, S1, S2), 
+    holds(F1, S1), holds(F2, S2).
+
+% formula satisfaction
+holds(wf, S) :- wf(S).
+holds(emp, s([],_R)).
+holds(not(F), S) :- \+ holds(F, S).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% continuous reasoning  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+crSep(s(P,R), s(Pok,Rok), s(Pko,Rko)) :-
+    partition(componentOk(s(P,R)), P, Pok, Pko),
+    ok(Pok, R, Rok), ko(R, Rok, Rko).
+
+cr(s(P, R), Sok, Sko, Sok) :-
+    crSep(s(P, R), Sok, Sko), holds(emp, Sko), wf(Sok).
+cr(s(P, R), s(Pok,Rok), s(PkoFixed,Rko), s(Pnew, R)) :-
+    crSep(s(P, R), s(Pok,Rok), s(Pko,Rko)), 
+    holds(wf ★ not(wf), s(P, R), s(Pok,Rok), s(Pko,Rko)),  writeln('Repairing:'), writeln(s(Pko,Rko)),
+    repair(Pko, Rko, PkoFixed),
+    append(Pok, PkoFixed, Ptmp), sort(Ptmp, Pnew),      
+    wf(s(Pnew,R)).
+
+repair(Pko, Rko, PkoFixed) :-
+    repairComponents(Pko, Rko, [], PAcc), 
+    sort(PAcc, PkoFixed).
+
+repairComponents([c(C,_)|Rest], Rko, PAcc, PFinal) :-
+    node(N),  delta(C, N),
+    req_hw(C, H), cap(N, Rko, CapN),
+    used_on_node(PAcc, N, Used),
+    CapN - Used >= H,
+    repairComponents(Rest, Rko, [c(C,N)|PAcc], PFinal).
+repairComponents([], _, P, P).
+
+componentOk(S, c(C,N)) :- nodeOk(N, S), delta(C, N).
+
+%%%%%%%%%%%%%%%% Utilities %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ok(Pok, _R, Rok) :- nodes(Ns), findall(r(N,Used), ( member(N, Ns), used_on_node(Pok, N, Used) ), Rok).
+
+ko(R, Rok, Rko) :- findall(r(N,CapKo),( member(r(N,Cap), R), cap(N, Rok, Used), CapKo is Cap - Used ), Rko).
+  
+used_on_node(P, N, Used) :- findall(H, ( member(c(C,N), P), req_hw(C, H) ), Hs), sum_list(Hs, Used).
+
+components(Cs) :- findall(C, component(C), Cs).
+
+nodes(Ns) :- findall(N, node(N), Ns).
+
+cap(N, R, C) :- member(r(N,C), R).
+
+delta(C,N) :- component(C), node(N).
+
+% generating resource separations %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+part([X|Xs], [X|L1], L2) :-
+    part(Xs, L1, L2).
+part([X|Xs], L1, [X|L2]) :-
+    part(Xs, L1, L2).
+part([], [], []).
+
+split([r(N,C)|Rs], [r(N,C1)|R1], [r(N,C2)|R2]) :-
+    between(0, C, C1), Max2 is C - C1, between(0, Max2, C2),
+    split(Rs, R1, R2).
+split([], [], []).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
